760fb3bdb3f4d2fe7d14618d260d2331b644fecb
Patrick Crouse <pat@Patricks-iMac.home>
2020-03-15 09:12:59 -0400

    functional profile page

--- app/routes.py
+++ app/routes.py
@@ -48,6 +48,17 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
