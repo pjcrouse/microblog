db83624f0b96aaf0b2cd7dcd71087b06bb65d6d9
Patrick Crouse <pat@Patricks-iMac.home>
2020-03-16 23:00:24 -0400

    email through aws only

--- app/__init__.py
+++ app/__init__.py
@@ -38,23 +38,7 @@ def create_app(config_class=Config):
     app.register_blueprint(main_bp)
 
     if not app.debug and not app.testing:
-        # email out severe issues
-        if app.config['MAIL_SERVER']:
-            auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-            secure = None
-            if app.config['MAIL_USE_TLS']:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='QuaranTogether Failure',
-                credentials=auth, secure=secure)
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
-
-         # log to file other less severe issues
+        # log to file other less severe issues
         if not os.path.exists('logs'):
             os.mkdir('logs')
         file_handler = RotatingFileHandler('logs/quarantogether.log', maxBytes=10240,
