ba014b8c9582d9bf84b705efd4abee0174eca5fc
Patrick Crouse <pat@Patricks-iMac.home>
2020-03-16 22:55:46 -0400

    functional app factory

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-from app import db
+from app import db, login
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
-from app import login, app
 from hashlib import md5
 from time import time
 import jwt
+from flask import current_app
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -66,12 +66,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
